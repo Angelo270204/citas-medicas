995c70408ff4e974cd95d0176088c47cddbb2f0b
Angelo270204 <angelow.piscis.27@gmail.com>
2026-02-11 23:59:08 -0500

    Creacion del AuthService, se agrego UserDetailsService y UserDetails en sus respectivas clases

---
+++ src/main/java/com/development/citasmedicas/domain/user/AuthService.java
@@ -0,0 +1,23 @@
+package com.development.citasmedicas.domain.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+        
+        return new SecurityUser(user);
+    }
+}
--- src/main/java/com/development/citasmedicas/domain/user/SecurityUser.java
+++ src/main/java/com/development/citasmedicas/domain/user/SecurityUser.java
@@ -1,7 +1,7 @@
 package com.development.citasmedicas.domain.user;
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -11,41 +11,45 @@ public class SecurityUser implements UserDetails {
     private final User user;
 
     public SecurityUser(User user){
-        this.user=user;
+        this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
     }
 
     @Override
-    public @Nullable String getPassword() {
-        return "";
+    public String getPassword() {
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return user.isEnable();
+    }
+
+    public User getUser() {
+        return user;
     }
 }
